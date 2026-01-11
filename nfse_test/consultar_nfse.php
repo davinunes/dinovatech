1339aa51242e0211b089437e205b7395b8889c13
ilunne <davi.nunes@gmail.com>
2026-01-11 09:17:57 -0300

    teste

--- nfse_test/consultar_nfse.php
+++ nfse_test/consultar_nfse.php
@@ -4,14 +4,14 @@
 // URL 1: Homologação Dados Oficiais (Blocked 403)
 // $endpoint_homolog = 'https://www.issnetonline.com.br/apresentacao/df/webservicenfse204/nfse.asmx';
 
-// URL 2: Homologação Fictícia (Returns 500 Client Error - likely structure)
+// URL 2: Homologação Fictícia (Using this one)
 $endpoint_homolog = 'https://www.issnetonline.com.br/homologaabrasf/webservicenfse204/nfse.asmx';
 
 $certificado_pfx = __DIR__ . '/../certificado/DInovaTech_1001347811.pfx';
 
 require __DIR__ . '/../certificado/certificado.php';
 
-echo "--- STARTING DEBUG SCRIPT (Fictitious Endpoint - Standardized Prefixes) ---\n";
+echo "--- STARTING DEBUG SCRIPT (Variation Test) ---\n";
 
 if (!file_exists($certificado_pfx)) {
     die("Error: Certificate file not found at $certificado_pfx\n");
@@ -23,35 +23,59 @@ if (!openssl_pkcs12_read($pfxContent, $certs, $senhaCertificado)) {
     die("Error requesting certificate. Check the password.\n");
 }
 
-echo "Certificate Loaded.\n";
+// --- VARIATION SETTINGS ---
+// USER: Change the values below to test with CPF if needed.
+$useCpf = false; // Set to true to use CPF instead of CNPJ
+$userCpf = '00000000000'; // PUT THE CPF HERE IF TESTING
 
+$queryType = 'PERIOD'; // Options: 'NUMBER' or 'PERIOD'
+
+// Data
 $cnpj = '61733714000101';
 $inscricaoMunicipal = '0841147200111';
 $numeroNota = '1';
+$dataCompetencia = '2026-01-11';
+$dataInicial = '2026-01-01';
+$dataFinal = '2026-01-31';
 
-// XML Construction with precise Namespace management
-// Payload uses http://www.abrasf.org.br/nfse.xsd (Default)
+echo "Mode: Cnpj=" . ($useCpf ? "CPF($userCpf)" : $cnpj) . ", QueryType=$queryType\n";
 
-$pedidoContent = <<<XML
-<Pedido>
-    <Prestador>
-        <CpfCnpj>
-            <Cnpj>$cnpj</Cnpj>
-        </CpfCnpj>
-        <InscricaoMunicipal>$inscricaoMunicipal</InscricaoMunicipal>
-    </Prestador>
-    <NumeroNfse>$numeroNota</NumeroNfse>
-    <Pagina>1</Pagina>
-</Pedido>
-XML;
+// XML Construction
+$pedidoContent = "<Pedido>";
+$pedidoContent .= "<Prestador>";
+$pedidoContent .= "<CpfCnpj>";
+
+if ($useCpf) {
+    $pedidoContent .= "<Cpf>$userCpf</Cpf>";
+} else {
+    $pedidoContent .= "<Cnpj>$cnpj</Cnpj>";
+}
+
+$pedidoContent .= "</CpfCnpj>";
+$pedidoContent .= "<InscricaoMunicipal>$inscricaoMunicipal</InscricaoMunicipal>";
+$pedidoContent .= "</Prestador>";
+
+if ($queryType === 'NUMBER') {
+    $pedidoContent .= "<NumeroNfse>$numeroNota</NumeroNfse>";
+} else {
+    // PeriodoCompetencia
+    $pedidoContent .= "<PeriodoCompetencia>";
+    $pedidoContent .= "<DataInicial>$dataInicial</DataInicial>";
+    $pedidoContent .= "<DataFinal>$dataFinal</DataFinal>";
+    $pedidoContent .= "</PeriodoCompetencia>";
+}
+
+$pedidoContent .= "<Pagina>1</Pagina>";
+$pedidoContent .= "</Pedido>";
+
 
 // Construct Root
 $rootXml = '<ConsultarNfseServicoPrestadoEnvio xmlns="http://www.abrasf.org.br/nfse.xsd">' . $pedidoContent . '</ConsultarNfseServicoPrestadoEnvio>';
 
-// Sign Root with 'ds' prefix
+// Sign Root
 $signedXml = assinarRoot($rootXml, $certs);
 
-// Remove headers just in case
+// Remove dangerous XML decls
 $search1 = '<' . '?xml version="1.0" encoding="UTF-8"?' . '>';
 $search2 = '<' . '?xml version="1.0"?' . '>';
 $signedXml = str_replace($search1, '', $signedXml);
@@ -86,7 +110,7 @@ curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $soapEnvelope);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
     'Content-Type: text/xml; charset=utf-8',
-    'SOAPAction: "http://nfse.abrasf.org.br/ConsultarNfseServicoPrestado"',
+    'SOAPAction: "http://nfse.abrasf.org.br/ConsultarNfseServicoPrestado"', // Service Namespace
     'Content-Length: ' . strlen($soapEnvelope),
     'Accept: text/xml',
     'Accept-Language: pt-BR',
@@ -97,13 +121,9 @@ curl_setopt($ch, CURLOPT_SSLCERT, $certPemFile);
 curl_setopt($ch, CURLOPT_SSLKEY, $keyPemFile);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($ch, CURLOPT_VERBOSE, true);
+// curl_setopt($ch, CURLOPT_VERBOSE, true); // Less verbose to focus on XML output if succeess
 
 $response = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-echo "\n--- REQUEST ENDED ---\n";
-echo "HTTP STATUS: $httpCode\n";
 
 if (curl_errno($ch)) {
     echo 'Curl Error: ' . curl_error($ch) . "\n";
@@ -133,10 +153,7 @@ function assinarRoot($xmlString, $certs)
     $canonicalized = $dom->C14N(false, false, null, null);
     $digestValue = base64_encode(sha1($canonicalized, true));
 
-    // Construct SignedInfo with 'ds' Prefix
-    // VERY IMPORTANT: XML Digital Signature standard often requires prefixes 'ds'
-    // and the declarations xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
-
+    // SignedInfo with ds prefix
     $signedInfo = '<ds:SignedInfo xmlns:ds="http://www.w3.org/2000/09/xmldsig#">' .
         '<ds:CanonicalizationMethod Algorithm="http://www.w3.org/TR/2001/REC-xml-c14n-20010315"></ds:CanonicalizationMethod>' .
         '<ds:SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"></ds:SignatureMethod>' .
@@ -157,7 +174,7 @@ function assinarRoot($xmlString, $certs)
     $signatureValue = '';
     openssl_sign($canonicalSignedInfo, $signatureValue, $certs['pkey'], OPENSSL_ALGO_SHA1);
     $signatureValueContent = base64_encode($signatureValue);
-    $signatureValueContent = chunk_split($signatureValueContent, 76, "\n"); // Some strict validations require wrapping
+    $signatureValueContent = chunk_split($signatureValueContent, 76, "\n");
 
     $x509 = str_replace(['-----BEGIN CERTIFICATE-----', '-----END CERTIFICATE-----', "\r", "\n"], '', $certs['cert']);
 
