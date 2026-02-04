5abba299e410a513137fb2a2fd073b6dcf2c7157
ilunne <davi.nunes@gmail.com>
2026-02-04 20:12:05 -0300

    fix test

--- dinovatech/app.php
+++ dinovatech/app.php
@@ -1866,7 +1866,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             // Append Footer
             // Note: User requested literal \s\n for line break. This is specific to this API.
-            $discriminacaoFinal .= "\s\nConforme documento auxiliar de cobranca numero " . $fatura['f_id'];
+            $discriminacaoFinal .= " Conforme documento auxiliar de cobranca numero " . $fatura['f_id'];
 
             $inputApi = [
                 'cnpj' => $config['cnpj'],
