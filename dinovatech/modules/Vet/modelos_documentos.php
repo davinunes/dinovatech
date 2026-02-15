1f7a77b5b269ff169a40a24c7f2500de074890b6
ilunne <davi.nunes@gmail.com>
2026-02-15 17:15:03 -0300

    fix

--- dinovatech/modules/Vet/modelos_documentos.php
+++ dinovatech/modules/Vet/modelos_documentos.php
@@ -18,7 +18,7 @@ if (!AppHelper::isVetMode()) {
 <head>
     <title>Modelos de Documentos - DinoVet</title>
     <?php include '../../components/layout_head.php'; ?>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/6.8.2/tinymce.min.js" referrerpolicy="origin"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <style>
         .variable-tag {
